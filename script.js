25e757c48c4cc8e74710a08f8a28ccdb3e0abd51
unknown <blancardt@merrimack.edu>
2023-07-14 15:45:17 -0400

    created the function that asks the user for the password length until a correct prompt is given or the user hits cancel.

--- script.js
+++ script.js
@@ -3,14 +3,63 @@
 function generatePassword() {
   //ask user for length of password.
   var length = passwordLength();
-  //get an array of boolean values to determine what charaters the user wants.
-  //bool array = [lower case, uppercase, numbers, special characters]
-  var characters = passwordCharaterTypes();
-  //ask user to confirm their selections
-  passwordSelectionConfirmation(length, characters);
 
-  //create password
-  return passwordCreation(length, characters);  
+  //check if user inputed a length
+  if (length !== null) {
+    //get an array of boolean values to determine what charaters the user wants.
+    //bool array = [lower case, uppercase, numbers, special characters]
+    var characters = passwordCharaterTypes();
+    //ask user to confirm their selections
+    passwordSelectionConfirmation(length, characters);
+  
+    //create password
+    return passwordCreation(length, characters); 
+  } 
+  else {
+    //if user cancels and this function does not return null than the textbox on the screen will display undefined.
+    //this way it will still say "Your Secured Password" or the previous password if the user cancels.
+    return length;
+  }
+}
+
+//create function to ask user for the length of the password.
+function passwordLength() {
+  //run an infinite loop until a user inputs a correct prompt or hits cancel on the first prompt of the loop.
+  //loop will end when it hits a line that returns a value.
+  //I have used while loops in my past and there was a bonus asking us about while loops.
+  while(true)
+  {
+    //give promp and ask for user input.
+    var length = window.prompt("How many characters would you like your password to contain.\nPlease input a number between 8 and 128");
+
+    //Check if user cancelled the prompt.
+    if (length !== null) {
+      //validation that it is a number between 8 and 128.
+      //Went to https://stackoverflow.com/questions/18042133/check-if-input-is-number-or-letter-javascript to find info on how to check 
+      //if user input is a number or a charater/string.  Also credited in the README file.
+      if (isNaN(length)) {
+        window.alert("Password length must be a number.  Please try again.");
+      }
+      else if (length < 8) {
+        window.alert("Password length must be at least 8 character.  Please try again.");
+      }
+      else if (length > 128) {
+        window.alert("Password length must be less than 128 character.  Please try again.");
+      }
+      else {
+        return length;
+      }
+    }
+    else {
+      return null;
+    }
+  }
+}
+
+//create a function to ask the user a series of questions to determine how to generate the password.
+function passwordCharaterTypes() {
+  var test = window.confirm("hit ok for true and cancel for false.")
+  window.alert(test);
 }
 
 // Get references to the #generate element
